6fcd6d2cbcf978275f99d43d54cce139ebbba113
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-07-25 17:34:46 -0300

    Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from "react";
-
 import "./footer.css";
+import { FiTwitter, FiInstagram } from "react-icons/fi";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com/gonzalogdv">
+          <FiTwitter />
+        </a>
+        <a href="https://www.instagram.com/gonzalogdv/">
+          <FiInstagram />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;

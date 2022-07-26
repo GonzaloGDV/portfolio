709323ee0b95f335c7328f6d73ff330589d5be00
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-07-26 18:51:27 -0300

    CreateRoot, About responsive, footer with experience, square me photo

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,6 +13,9 @@ const Footer = () => {
           <a href="#about">About</a>
         </li>
         <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
           <a href="#portfolio">Portfolio</a>
         </li>
         <li>
@@ -21,10 +24,10 @@ const Footer = () => {
       </ul>
 
       <div className="footer__socials">
-        <a href="https://twitter.com/gonzalogdv">
+        <a href="https://twitter.com/gonzalogdv" target="_blank">
           <FiTwitter />
         </a>
-        <a href="https://www.instagram.com/gonzalogdv/">
+        <a href="https://www.instagram.com/gonzalogdv/" target="_blank">
           <FiInstagram />
         </a>
       </div>

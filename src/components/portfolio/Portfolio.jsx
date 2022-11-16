91aa41e367b35667a3ff8876ea3864f05de00165
Gonzalo Gomez <gonzalogdv@gmail.com>
2022-11-16 08:49:57 -0300

    circus portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,44 +1,52 @@
-import React from "react";
-import "./portfolio.css";
-import IMG1 from "../../assets/portfolio-dogs.png";
-import IMG2 from "../../assets/portfolio-codebug.png";
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/portfolio-dogs.png';
+import IMG2 from '../../assets/portfolio-codebug.png';
+import IMG3 from '../../assets/portfolio-circus.png';
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Dogs App",
-    github: "https://github.com/GonzaloGDV/PI-Dogs",
-    demo: "https://pi-dogs-gonzalogdv.vercel.app",
+    title: 'Dogs App',
+    github: 'https://github.com/GonzaloGDV/PI-Dogs',
+    demo: 'https://pi-dogs-gonzalogdv.vercel.app',
   },
   {
     id: 2,
     image: IMG2,
-    title: "CodeBug",
-    github: "https://github.com/Codebug-Henry/Co-debug",
-    demo: "https://codebug-ten.vercel.app",
+    title: 'CodeBug',
+    github: 'https://github.com/Codebug-Henry/Co-debug',
+    demo: 'https://codebug-ten.vercel.app',
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Circus Cotillón',
+    github: 'https://github.com/CotillonCircus/WebApp',
+    demo: 'https://circuscotillon.com',
   },
 ];
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">
+    <section id='portfolio'>
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
-      <div className="container portfolio__container">
+      <div className='container portfolio__container'>
         {data.map(({ id, image, title, github, demo }) => {
           return (
-            <article key={id} className="portfolio__item">
-              <div className="portfolio item_image">
+            <article key={id} className='portfolio__item'>
+              <div className='portfolio item_image'>
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
-              <div className="portfolio__item-cta">
-                <a href={github} className="btn" target="_blank">
+              <div className='portfolio__item-cta'>
+                <a href={github} className='btn' target='_blank'>
                   Github
                 </a>
-                <a href={demo} className="btn btn-primary" target="_blank">
+                <a href={demo} className='btn btn-primary' target='_blank'>
                   Live Demo
                 </a>
               </div>
